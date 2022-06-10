30ac5b2e5b331a76c5e76e592022dfe7466a6e40
mfcgames <mfethicaliskan@gmail.com>
2022-06-10 13:03:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+" İsimli Müşteri Başarıyla Eklendi.");
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+" "+musteri.Surname+" İsimli Müşteri Silindi.");
+        }
+        public void MusteriListele(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Name+ " " + musteri.Surname);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri[] musteri = new Musteri[3];
+            musteri[0] = new Musteri();
+            musteri[1] = new Musteri();
+            musteri[2] = new Musteri();
+            musteri[0].Name = "Ahmet";
+            musteri[0].Surname = "Mantıksız";
+            musteri[0].Id = 1;
+            musteri[1].Name = "Murtaza";
+            musteri[1].Surname = "Yumurta";
+            musteri[1].Id = 2;
+            musteri[2].Id = 3;
+            musteri[2].Name = "Yeliz";
+            musteri[2].Surname = "Yeşilmen";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri[0]);
+            musteriManager.MusteriListele(musteri[1]);
+            musteriManager.MusteriSil(musteri[2]);
+
+        }
+    }
+}
